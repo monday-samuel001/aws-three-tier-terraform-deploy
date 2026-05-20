690326e9b539e9c39dbc17b0140bd2724c595a6f
samuel <mondaysamueludoh@gmail.com>
2026-05-20 01:17:46 +0100

    completed

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "digitalwitchngbucketcloud1"
-    key    = "digitalwitchng/prodution/terraform.tfstate"
-    region = "us-west-1"
+    bucket = "samuel-terraform-state-123"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
